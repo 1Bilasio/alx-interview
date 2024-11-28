496feb85baab168b599548abd6476abce8d47a62
1Bilasio <sirbilasio@gmail.com>
2024-11-28 23:46:18 +0300

    0x08. Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-##Making Change
+##Change making module
 
 def makeChange(coins, total):
-    """
-    Return the minimum number of coins needed to meet a given total
-    Args:
-        coins (list of ints): a list of coins of different values
-        total (int): total value to be met
-    Return:
-        Number of coins or -1 if meeting the total is not possible
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
     if total <= 0:
         return 0
-    if coins == [] or coins is None:
-        return -1
-    try:
-        n = coins.index(total)
-        return 1
-    except ValueError:
-        pass
-
-    coins.sort(reverse=True)
-    coin_count = 0
-    for i in coins:
-        if total % i == 0:
-            coin_count += int(total / i)
-            return coin_count
-        if total - i >= 0:
-            if int(total / i) > 1:
-                coin_count += int(total / i)
-                total = total % i
-            else:
-                coin_count += 1
-                total -= i
-                if total == 0:
-                    break
-    if total > 0:
-        return -1
-    return coin_count
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
