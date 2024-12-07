1b46f23a507ee3712cffad5fe3f0ca8f3b2a2d9d
1Bilasio <sirbilasio@gmail.com>
2024-12-07 14:48:14 +0300

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-##Solution to lockboxes problem
+
+"""Function opens a stack of boxes and returns true if all opened
+and false if not all opened"""
+
 
 def canUnlockAll(boxes):
-    """
-    Determines whether a series of locked boxes can be opened
-    based on keys that can be attained.
-    Solution to the lockboxes problem
-    """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
-        return False
+    """Function that checks if n number of boxes can be all opened
+    Returns: True if they are all successfully opened and False if not"""
 
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    # create a set for rhe opened boxes with the first box
+    opened_boxes = {0}
+    # make a shallow copy stack of opened box 0 content
+    list_1 = boxes[0].copy()
+
+    # check if there is the copy is not empty and pop the last to use
+    while list_1:
+        key = list_1.pop()
+        # check if key matches a box and not used yet
+        if key not in opened_boxes and key < len(boxes):
+            # Add it to the set of opened boxes
+            opened_boxes.add(key)
+            # Add all keys found to the stack
+            list_1.extend(boxes[key])
+    if len(opened_boxes) == len(boxes):
+        return True
+    else:
+        return False
